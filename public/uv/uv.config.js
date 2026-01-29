9bd1e12d920c3c9b1a2c571134860bb7fe94b933
Crest5005 <rancrest0@gmail.com>
2026-01-28 17:38:03 -0800

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,6 +1,9 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://uv.student-portal.workers.dev/', // ADD THIS LINE
+    // The Bare server (engine for basic sites)
+    bare: 'https://uv.student-portal.workers.dev/', 
+    // The Wisp server (engine for YouTube/Twitch/WebSockets)
+    wisp: 'wss://wisp.mercurywork.shop/', 
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
