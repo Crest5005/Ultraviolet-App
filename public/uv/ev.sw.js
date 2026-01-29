92cd345999ccb214ea5c229441992ecb9366f8d7
Crest5005 <rancrest0@gmail.com>
2026-01-28 17:50:07 -0800

    Create ev.sw.js

---
+++ public/uv/ev.sw.js
@@ -0,0 +1,9 @@
+importScripts('./uv.bundle.js');
+importScripts('./uv.config.js');
+importScripts(__uv$config.sw || './uv.sw-handler.js');
+
+const sw = new UVServiceWorker();
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(sw.fetch(event));
+});
