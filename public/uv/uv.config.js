49b9b5a72824262994e936b8f517076d6ba6c332
Crest5005 <rancrest0@gmail.com>
2026-01-28 17:56:18 -0800

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,7 +1,7 @@
 self.__uv$config = {
     prefix: '/service/',
     bare: 'https://uv.student-portal.workers.dev/', 
-    wisp: 'wss://wisp.mercurywork.shop/', // This is the most stable 2026 link
+    wisp: 'wss://wisp.mercurywork.shop/', 
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
