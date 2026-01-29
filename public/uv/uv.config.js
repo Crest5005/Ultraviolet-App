d01be26e63ec24489c68f86e5afbdd840ada19b8
Crest5005 <rancrest0@gmail.com>
2026-01-28 18:45:04 -0800

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,11 +1,9 @@
-/*global Ultraviolet*/
+// Inside uv/uv.config.js
 self.__uv$config = {
     prefix: '/service/',
-    encodeUrl: Ultraviolet.codec.xor.encode,
-    decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/uv.handler.js',
-    client: '/uv.client.js',
-    bundle: '/uv.bundle.js',
-    config: '/uv.config.js',
-    sw: '/uv.sw.js',
+    bare: 'https://uv.student-portal.workers.dev/', 
+    handler: '/uv/uv.handler.js', 
+    bundle: '/uv/uv.bundle.js', 
+    config: '/uv/uv.config.js',
+    sw: '/uv/uv.sw.js',
 };
